f564e91813e12ba7ae19f842387e9fdeb31c46f7
J Farmer <JFarm0>
2021-05-30 10:16:55 -0500

    Coding Events Exercises, Create a Model Class Complete 12.2.1

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,44 +1,36 @@
 package org.launchcode.codingevents.controllers;
 
+import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 @Controller
 @RequestMapping("events")
 public class EventController {
 
-//    private static List<String> events = new ArrayList<>();
+    private static List<Event> events = new ArrayList<>();
 
     @GetMapping()
     public String displayAllEvents(Model model) {
-        HashMap<String, String> events = new HashMap<>();
-        events.put("Coding with Pride", "Welcoming coders in the LGBTQ community!");
-        events.put("WWDC", "It's some Apple Event.");
-        events.put("StlGDC", "It's the St. Louis Game Developer's Conference.");
-        //        List<String> events = new ArrayList<>();
-//        events.add("Code With Pride");
-//        events.add("Strange Loop");
-//        events.add("Apple WWDC");
-//        events.add("SpringOne Platform");
-//        model.addAttribute("events", events);
-//        return "events";
+        model.addAttribute("title", "All Events");
         model.addAttribute("events", events);
         return "events/index";
     }
 
-    // lives at /events/create
     @GetMapping("create")
-    public String renderCreateEventForm() {
+    public String displayCreateEventForm(Model model) {
+        model.addAttribute("title", "Create Event");
         return "events/create";
     }
 
-    // also lives at /events/create
-//    @PostMapping("create")
-//    public String createEvent(@RequestParam String eventName) {
-//        events.add(eventName);
-//        return "redirect:";
-//    }
+    @PostMapping("create")
+    public String processCreateEventForm(@RequestParam String eventName) {
+        events.add(new Event(eventName));
+        return "redirect:";
+    }
 }
---
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -0,0 +1,23 @@
+package org.launchcode.codingevents.models;
+
+public class Event {
+
+    private String name;
+
+    public Event(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
