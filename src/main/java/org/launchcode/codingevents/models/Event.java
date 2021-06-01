a6deca1be59ac6128dbe3d22a2eb1f24b05b22a9
J Farmer <JFarm0>
2021-05-31 21:40:23 -0500

    Coding Events Exercises, create data layer Complete 12.2.3

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,6 +1,11 @@
 package org.launchcode.codingevents.models;
 
+import java.util.Objects;
+
 public class Event {
+
+    private int id;
+    private static int nextId = 1;
 
     private String name;
     private String description;
@@ -8,6 +13,8 @@ public class Event {
     public Event(String name, String description) {
         this.name = name;
         this.description = description;
+        this.id = nextId;
+        nextId++;
     }
 
     public String getName() {
@@ -26,8 +33,25 @@ public class Event {
         this.description = description;
     }
 
+    public int getId() {
+        return id;
+    }
+
     @Override
     public String toString() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Event event = (Event) o;
+        return id == event.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
